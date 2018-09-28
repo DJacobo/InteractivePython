6fd0af74f6de80744ad5fc6215419a3179600ad9
DJacobo <djacobo772@gmail.com>
2018-09-28 00:55:14 -0700

    Implement Deque (unfinished)

---
+++ IP_Ch3_17_ImplementingDeque.py
@@ -0,0 +1,40 @@
+class Deque():
+    def __init__(self):
+        self.items = []
+
+    def addFront(self, item):
+        self.items.insert(len(self.items), item)
+    
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+d = Deque()
+print(d.isEmpty())
+d.addRear(4)
+print(d)
+d.addRear('dog')	
+print(d) 
+d.addFront('cat')
+print(d)
+d.addFront(True)
+print(d)
+print(d.size())
+print(d.isEmpty())
+d.addRear(8.4)
+print(d)
+print(d.removeRear())
+print(d)
+print(d.removeFront())
+print(d)
