f711bff8030392252f9ceb965ff7dae2b7bfde40
DJacobo <djacobo772@gmail.com>
2018-10-19 14:16:39 -0700

    come back todo these later

---
+++ IP_Ch4_15_DiscussionQuestions.py
@@ -0,0 +1,6 @@
+# Draw a call stack for the Tower of Hanoi problem. Assume that you start with a stack of three disks.
+# TODO:
+# Using the recursive rules as described, draw a Sierpinski triangle using paper and pencil.
+# TODO:
+# Using the dynamic programming algorithm for making change, find the smallest number of coins that you can use to make 33 cents in change. In addition to the usual coins assume that you have an 8 cent coin.
+# 2 coins, 8 and 25
